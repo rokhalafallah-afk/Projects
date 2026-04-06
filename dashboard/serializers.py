679f7271821c37575b0344424b9067b101e02713
ro.khalafallah-afk <ro.khalafallah@gmail.com>
2026-04-06 23:49:14 +0200

    Changed DB into PostGress

---
+++ dashboard/serializers.py
@@ -0,0 +1,28 @@
+# dashboard/serializers.py
+from rest_framework import serializers
+from .models import Registration, User, Event, News, Match  # عدلي حسب أسماء الـ Models عندك
+
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+class NewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = News
+        fields = '__all__'
+
+class SportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Match
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
